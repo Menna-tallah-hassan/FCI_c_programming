bb50ac60d7744adfb315e1702510c53278d3bb8a
Menna Tallah Hassan <85581099+Menna-tallah-hassan@users.noreply.github.com>
2021-06-08 21:42:00 +0200

    Add files via upload

---
+++ decimal.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n;
+    for ( int i = 1 ; i <=5 ; i++ ){
+            for(int j=9;j>=1;j--){
+                printf("*");
+            }
+
+        }
+    return 0;
+}
+
